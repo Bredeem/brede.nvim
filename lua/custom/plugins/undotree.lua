70aedf977f76550239719567e60406ca1bb9c897
Brede Eder Murberg <brede@murberg.no>
2025-10-06 20:26:15 +0200

    rebind keys and add undotree

---
+++ lua/custom/plugins/undotree.lua
@@ -0,0 +1,5 @@
+return {
+  { 'mbbill/undotree', keys = {
+    { '<leader>F4', vim.cmd.UndotreeToggle, desc = 'Toggle Undotree' },
+  } },
+}
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,6 +8,8 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+vim.keymap.set('i', 'jk', '<Esc>')
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
@@ -32,6 +34,7 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
+
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
